ab38a1d18390018cc12701613fdeb0188eab4fad
jiangwang.foo <jiangwang.foo@bytedance.com>
2022-10-20 16:39:32 +0800

    main - mywork -1 -- 2

--- src/main/java/com/example/springbootfoo/controller/ServiceA.java
+++ src/main/java/com/example/springbootfoo/controller/ServiceA.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 public class ServiceA {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.addAll(null);
-        System.out.println(list);
-        System.out.println("这里是建了workf分支后加入的内容----");
-        System.out.println("main - mywork -1 -- 2");
+        String s1 = "";
+        String s2 = null;
+        System.out.println(s1.equals(s2));
+
+
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.addAll(null);
+//        System.out.println(list);
+//        System.out.println("这里是建了workf分支后加入的内容----");
+//        System.out.println("main - mywork -1 -- 2");
     }
 }
